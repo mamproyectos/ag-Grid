750dc31e9a1b9285c77f484319b48b295cdc21d6
mamjerez <correomam@gmail.com>
2019-09-15 05:19:18 +0200

    Solo paraavanzar esta rama.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AgGridAngular } from 'ag-grid-angular';
 
+
 const valueCellStyle = {
   'text-align': 'right'
 };
